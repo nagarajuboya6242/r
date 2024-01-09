5f44003c98884c3e1d8e449b0a06efcad2e6f466
nagaraj <rowdynaga1432@gmail.com>
2024-01-09 10:13:27 +0000

    solution

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const Contact = () => (
+  <div className="contact-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="contact"
+      className="contact-img"
+    />
+    <h1 className="contact-heading">Contact</h1>
+  </div>
+)
+
+export default Contact
